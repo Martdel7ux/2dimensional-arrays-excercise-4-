75fc69b626289fa0a458acdd65436fa295fb1ae5
Martin Ahoto <martinahoto4@gmail.com>
2021-10-28 14:46:55 +0300

    Initial Commit

---
+++ 2dimensional arrays(excercise 4)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2dimensional arrays(excercise 4)
+//
+//  Created by MARTIN on 28/10/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
